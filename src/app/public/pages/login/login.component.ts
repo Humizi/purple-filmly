45843b23a75633915e6f2718f54585dd15ca3952
Humizi <humizi307@gmail.com>
2026-08-02 17:24:26 +0300

    add cva

--- src/app/public/pages/login/login.component.ts
+++ src/app/public/pages/login/login.component.ts
@@ -1,4 +1,11 @@
 import { Component, inject } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { catchError, of, take, tap } from 'rxjs';
 
 import { AuthService } from '../../../shared/services/auth.service';
@@ -11,29 +18,39 @@ import { Router } from '@angular/router';
   selector: 'app-login-component',
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
-  imports: [ButtonComponent, InputComponent, PasswordInputComponent],
+  imports: [
+    ButtonComponent,
+    InputComponent,
+    PasswordInputComponent,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   standalone: true,
 })
 export class LoginComponent {
   private _authService = inject(AuthService);
   private _router = inject(Router);
-  private _formValue: { username: string | null; password: string | null } = {
+  public formValue: { username: string | null; password: string | null } = {
     username: null,
     password: null,
   };
   error: string | null = null;
 
-  onInputChange(ctrl: 'username' | 'password', value: string | null): void {
-    this._formValue[ctrl] = value;
-  }
+  form = new FormGroup({
+    email: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.email],
+    }),
+    password: new FormControl('', { nonNullable: true }),
+  });
 
   onLoginClick(): void {
-    if (this._formValue.username === null || this._formValue.password === null) {
+    if (this.formValue.username === null || this.formValue.password === null) {
       return;
     }
 
     this._authService
-      .login$(this._formValue.username, this._formValue.password)
+      .login$(this.formValue.username, this.formValue.password)
       .pipe(
         take(1),
         tap(() => this._router.navigate(['private'])),
--- src/app/shared/components/password-input/password-input.component.ts
+++ src/app/shared/components/password-input/password-input.component.ts
@@ -1,6 +1,7 @@
-import { Component, input, output } from '@angular/core';
+import { Component, forwardRef, input, signal } from '@angular/core';
+import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR } from '@angular/forms';
 
-import { FormsModule } from '@angular/forms';
+import { noop } from 'rxjs';
 
 @Component({
   selector: 'app-password-input-component',
@@ -8,14 +9,23 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './password-input.component.scss',
   standalone: true,
   imports: [FormsModule],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => PasswordInputComponent),
+      multi: true,
+    },
+  ],
 })
-export class PasswordInputComponent {
-  placeholder = input('');
-  disabled = input(false);
-  icon = input<string | null>(null);
-  value = input<string | null>(null);
+export class PasswordInputComponent implements ControlValueAccessor {
+  private onChange: (value: string) => void = noop;
+  private onTouched: () => void = noop;
 
-  controlValue = output<string | null>();
+  placeholder = input('');
+  icon = input<string | null>(null);
+
+  disabled = signal(false);
+  value = signal('');
 
   type: 'text' | 'password' = 'password';
 
@@ -23,11 +33,32 @@ export class PasswordInputComponent {
     return this.type === 'password' ? 'icons/eye--closed.svg' : 'icons/eye--open.svg';
   }
 
-  onInput(value: string | null): void {
-    this.controlValue.emit(value);
+  writeValue(value: string): void {
+    this.value.set(value);
+  }
+
+  registerOnChange(fn: (value: string) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled.set(isDisabled);
   }
 
   toggleType(): void {
     this.type = this.type === 'text' ? 'password' : 'text';
+  }
+
+  handleInput(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    const value = target.value;
+
+    this.value.set(value);
+    this.onChange(value);
+    this.onTouched();
   }
 }
